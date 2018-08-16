7b6ef4feaf9b2c2a2df47a074d714cc699647a79
anshadapm <42177455+anshadapm@users.noreply.github.com>
2018-08-16 16:01:47 +0000

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,30 @@
+
+//include library
+#include <iostream>
+using namespace std;
+int main(){
+int a = 8;
+int b = 2;
+//add two integers
+int sum = a + b;
+//print sum on terminal
+cout << "The sum of "  << a <<  " and "  << b <<  " is "  << sum << endl;
+//find difference
+int difference = a - b;
+//print difference on terminal
+cout << "The difference of "  << a <<  " and "  << b <<  " is "  << difference << endl;
+//multiply two numbers
+int multiplication = a * b;
+//print multiplication on terminal
+cout << "The multiplication of "  << a <<  " and "  << b <<  " is "  << multiplication << endl;
+//divide numbers
+int divition = a / b;
+//print divition on terminal
+cout << "The divition of "  << a <<  " and "  << b <<  " is "  << divition << endl;
+//find modulus
+int modulus = a % b ;
+//print modulus on terminal
+cout << "The modulus of " << a << " and " << b << " is " << modulus << endl;
+return 1;
+}
+
