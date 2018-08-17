fd4010ef3a2e0494019a2063e249efc5e43ce321
anshadapm <42177455+anshadapm@users.noreply.github.com>
2018-08-17 05:20:54 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,24 @@
+//Library
+#include <iostream>
+using namespace std;
+int main(){
+   //declaring variables of all types
+   int a = 2;/*integer*/
+   char b = 'x';/*charecter*/
+   bool c = true;/*boolean*/
+   float d = 0.3;/*float*/
+   double e = 1.99;/*double*/
+    //declaration of output
+   cout<< "a = "<< a << endl;
+   cout<< "b = "<< b << endl;
+   cout<< "c = "<< c << endl;
+   cout<< "d = "<< d << endl;
+   cout<< "e = "<< e << endl;
+   //displaying the size of data types
+   cout<< "size of integer " << sizeof(a) << endl;
+   cout<< "size of charecter " << sizeof(b) << endl;
+   cout<< "size of boolean " << sizeof(c) << endl;
+   cout<< "size of float " << sizeof(d) << endl;
+   cout<< "size of double " << sizeof(e) << endl;
+}
+        
